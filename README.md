0fb2b90abae90f5d7da651750baebc57ca3e2e1c
niko1499 <nxgamarra@gmail.com>
2018-12-10 22:42:00 -0500

    add dithering script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# seurobot
+# Seurobot
 
 
 
@@ -6,6 +6,6 @@
 
 ## Launch files
 
-'''
+```
 roslaunch abb_irb1600_6_12_moveit_config moveit_planning_execution.launch sim:=false robot_ip:=192.168.100.100
-'''
+```
--- src/motion_planning_client/scripts/motion_planning_client.py
+++ src/motion_planning_client/scripts/motion_planning_client.py
@@ -8,21 +8,28 @@ import actionlib
 # import MoveRobot.msg
 # import motion_action_server.msg
 # import motion_msgs.msg  # import MoveRobotAction
+import os
+
+
+from PIL import Image
+
 
 import motion_msgs
 from motion_msgs.msg import MoveRobotAction
 from force_msgs.msg import LoadCellForces32
 
 class Robot:
+    zZ=.015
+    zA=.0225
+    zB=zZ
+    zC=zZ
+
     def __init__(self):
         self.client = actionlib.SimpleActionClient('motion',
                                               motion_msgs.msg.MoveRobotAction)
 
         self.force_sub=rospy.Subscriber('/force_data',LoadCellForces32,self.forceCB)
-        #self.Zz
-        #self.Az
-        #self.Bz
-        #self.Cz
+
 
     def forceCB(self,data):#function to prcess force data 
         print("forcecb working")
@@ -59,30 +66,63 @@ class Robot:
 
         # Waits for the server to finish performing the action.
         self.client.wait_for_result()
-        return self.client.get_result()  # A FibonacciResult
+        return self.client.get_result()
+def printImage():
+    print("in print image")
 
 
 def main_node():
 
     myRobot=Robot()
 
-    try:
-        # Initializes a rospy node so that the SimpleActionClient can
-        # publish and subscribe over ROS.
-        # x=-.5 y=-.6
-        #              x,  y,  z,  r,  p,  y,  tool
-        #z rotation off by 180 -28?
+    debug=False
 
-        #z .025 red
-        #z .0225 blue
 
-        result = myRobot.move(.4, -.7, .04, 0, 180, 0, "tool_green")
-        print(result)
+    if(debug==False):
+        print("debug is false")
+        boardx = -.4#rospy.get_param('workcell/canvas_x')
+        boardy = -.4#rospy.get_param('workcell/canvas_y')
+        boardz = rospy.get_param('workcell/canvas_z')
 
-    except rospy.ROSInterruptException:
-        print("program interrupted before completion", file=sys.stderr)
+        painter=Robot()
+        seperation=.01
+        zClearance=.3
+        zTouching=.015
+        currentTool="tool_red"
 
-        # Prints out the result of executing the action
+        scriptDir = os.path.dirname(__file__)
+        impath = os.path.join(scriptDir, '../../../../seurobot_ws/image_script/output.png')
+        print("reading image")
+        img = Image.open(impath)
+        img = img.convert('RGB')
+        width, height = img.size
+        print("Image done reading")
+        #painter.move(boardx, boardy, zClearance, 0 , 180, 0, currentTool)
+        for x in range(width):
+            for y in range(height):
+                r, g, b = img.getpixel((x,y))
+                print("\nPrinting:")
+                print("{}%".format(int(100.0 * y / height)))
+                print("On x=", x, "y=", y, "Color=",r, g, b)
+    else:
+        try:
+            # Initializes a rospy node so that the SimpleActionClient can
+            # publish and subscribe over ROS.
+            # x=-.5 y=-.6
+            #              x,  y,  z,  r,  p,  y,  tool
+            #z rotation off by 180 -28?
+
+            #z .025 red
+            #z .0225 blue
+
+            result = myRobot.move(0, -.7, .4, 0, 180, 0, "tool_green")
+
+            print(result)
+
+        except rospy.ROSInterruptException:
+            print("program interrupted before completion", file=sys.stderr)
+
+            # Prints out the result of executing the action
     rospy.spin()
 
 
